e64c626c423b25b72b18c0e4b742a19654f327ab
Mahmoud <mmatouk@users.noreply.github.com>
2020-03-24 13:08:36 -0500

    Update environment.prod.ts

--- webClient/angular/src/environments/environment.prod.ts
+++ webClient/angular/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  region: 'your region',
-  userPoolId: 'your user pool id',
-  userPoolWebClientId: 'your client id',
+  region: 'us-west-2',
+  userPoolId: 'us-west-2_VaWUbZ2ZA',
+  userPoolWebClientId: '188ah9odtgrorhpr8pspq924r1',
 };
